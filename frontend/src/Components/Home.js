422121670231f35df6767714899a5de48bcf46c5
shourov-sorkar <18103142@iubat.edu>
2021-02-23 22:47:34 +0600

    :tada: Start project, create layout and add coming soon page

---
+++ frontend/src/Components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Timer from "./Timer/Timer"
+function Home() {
+    return (
+        <div className="App">
+      <div className="container">
+        <h1>
+          Website
+          <br />
+          Coming Soon
+        </h1>
+        <Timer />
+      </div>
+    </div>
+    )
+}
+export default Home
